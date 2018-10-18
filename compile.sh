25c332350c1eb92fd0fe761c116b9c781f3c99f9
Jesse Hoobergs <jesse@hoobergs.be>
2018-10-18 12:11:47 +0200

    Update compile script for bibtex and some extra explanation

--- compile.sh
+++ compile.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-pdflatex -output-directory=results masterproef.tex
+rm -r results
+mkdir results
+cp *.tex results
+cp *.bib results
+cd results
+pdflatex masterproef
+bibtex masterproef
+pdflatex masterproef
+pdflatex masterproef
+rm *.tex *.bib
