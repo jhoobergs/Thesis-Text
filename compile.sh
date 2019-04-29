15a289e59761f38eba88bb5469c0c3cdc08fe5a1
Jesse Hoobergs <jesse@hoobergs.be>
2019-04-29 15:35:34 +0200

    Add basic layout

--- compile.sh
+++ compile.sh
@@ -3,9 +3,11 @@ rm -r results
 mkdir results
 cp *.tex results
 cp *.bib results
+cp kulemt* results
+cp logokul* results
 cd results
 pdflatex masterproef
 bibtex masterproef
 pdflatex masterproef
 pdflatex masterproef
-rm *.tex *.bib
+rm *.tex *.bib kulemt* logokul*
